217163bca2b96b43fadb63dc73d7162dbdd7e251
David Lee Clark <davetheclark87@gmail.com>
2022-09-20 02:07:36 -0600

    dave6

--- daveinvaders/src/html.js
+++ daveinvaders/src/html.js
@@ -168,116 +168,328 @@ class Html extends Component {
         <h2>D</h2>
 
         <code>
-
+        datalist-	Specifies an autocomplete feature to be used with a form element
         </code>
 
         <code>
-        
+        dd-	Describes a term in a description list
         </code>
 
         <code>
-        
+        del-	Defines deleted text in a document and crosses it out
         </code>
 
         <code>
-        
+        details-	Specifies additional details that can be shown or hidden manually
         </code>
 
         <code>
-        
+        dfn-	Represents a term that needs an explanation
         </code>
 
         <code>
-        
+        dialog-	Creates pop-up dialog models
+        </code>
+
+        <code>
+        dir-	DEPRECATED. Defined a directory list
+        </code>
+
+        <code>
+        div-	Defines a section or a division in HTML document by grouping elements
+        </code>
+
+        <code>
+        dl-	Defines a description list
+        </code>
+
+        <code>
+        dt-	Defines a description term
         </code>
 
         <h2>E</h2>
 
         <code>
-
+        em-	Defines emphasized text
         </code>
 
         <code>
-        
-        </code>
-
-        <code>
-        
-        </code>
-
-        <code>
-        
-        </code>
-
-        <code>
-        
-        </code>
-
-        <code>
-        
+        embed-	Defines an embedded resource
         </code>
 
         <h2>F</h2>
 
         <code>
-
+        fieldset-	Defines a group of form elements
         </code>
 
         <code>
-        
+        figcaption-	Sets a caption for a figure element
         </code>
 
         <code>
-        
+        figure-	Defines a self-contained piece of content
         </code>
 
         <code>
-        
+        font-	DEPRECATED. Defined text style
         </code>
 
         <code>
-        
+        footer-	Defines footer context
         </code>
 
         <code>
-        
+        form-	Defines a group of submittable inputs
         </code>
 
-        <h2>G</h2>
+        <code>
+        frame-	DEPRECATED. Defined one single frame in a web page
+        </code>
 
         <code>
-
+        frameset-	DEPRECATED. Defined a container for holding multiple frames
         </code>
 
         <h2>H</h2>
 
         <code>
+        h1 - h6	Defines headings in a document
+        </code>
 
+        <code>
+        head-	Defines a space for declaring title, stylesheets, scripts and other document related information
+        </code>
+
+        <code>
+        header-	Specifies a container for one or more headings
+        </code>
+
+        <code>
+        hr	Defines a thematic division between content
+        </code>
+
+        <code>
+        html-	Defines a document as an HTML document
         </code>
 
         <h2>I</h2>
-
+        
         <code>
-
+        i-	Defines an italic styled text
         </code>
 
-        <h2>J</h2>
+        <code>
+        iframe-	Defines a frame of inline nature
+        </code>
 
         <code>
-
+        img-	Sets an area for an image
         </code>
+
+        <code>
+        input-	Specifies an element used to take values from the user
+        </code>
+
+        <code>
+        ins-	Defines inserted text in a document
+        </code>
+
 
         <h2>K</h2>
 
         <code>
-
+        kbd-	Identifies a user keyboard entry
         </code>
-
-        <h2>Language</h2>
 
         <code>
-
+        keygen-	DEPRECATED. Defined a key-pair used while submitting an HTML form
         </code>
+
+        <h2>L</h2>
+
+        <code>
+        label-	Specifies a label for a given input element
+        </code>
+
+        <code>
+        legend-	Sets a caption for the fieldset
+        </code>
+
+        <code>
+        li-	Defines a list item within a list
+        </code>
+
+        <code>
+        link-	Sets a link between a document and other external resources
+        </code>
+
+        <h2>M</h2>
+
+        <code>
+        main-	Defines the primary content of the document
+        </code>
+
+        <code>
+        map-	Defines a client-side image-map (an image with clickable places)
+        </code>
+
+        <code>
+        mark-	Defines marked text
+        </code>
+
+        <code>
+        menu-	Defines a list/menu of commands
+        </code>
+
+        <code>
+        menuitem-	DEPRECATED. Defined a command, which can be invoked from the popup menu by the user
+        </code>
+
+        <code>
+        meta-	Defines supplementary information about a website
+        </code>
+
+        <code>
+        meter-	Displays a scalar measurement within a known range
+        </code>
+
+        <h2>N</h2>
+
+        <code>
+        nav-	Defines a block of navigational links to the main sections of a website
+        </code>
+
+        <code>
+        noframes-	DEPRECATED. Defined fallback content for the browsers which did not support the frame tag
+        </code>
+
+        <code>
+        noscript-	Defines HTML to be inserted as replacement if the browser does not support scripting
+        </code>
+
+
+        <h2>O</h2>
+
+        <code>
+        object-	Defines an external source that is embedded into the HTML document
+        </code>
+
+        <code>
+        ol-	Defines an ordered list of items in the HTML document
+        </code>
+
+        <code>
+        optgroup-	Groups the related options within a select element
+        </code>
+
+        <code>
+        option-	Sets an HTML option in select element list
+        </code>
+
+        <code>
+        output-	Displays the result of a calculation performed
+        </code>
+
+        <h2>P</h2>
+        
+        <code>
+        p-	Defines an HTML paragraph of text
+        </code>
+
+        <code>
+        param-	Defines parameters for an object element
+        </code>
+
+        <code>
+        pre-	Defines preformatted text. Preserves spaces and line breaks
+        </code>
+
+        <code>
+        progress-	Displays an HTML progress bar representing the progress of a task
+        </code>
+
+        <h2>Q</h2>
+
+        <code>
+        q-	Indicates that the enclosed text is an inline quotation
+        </code>
+
+        <h2>R</h2>
+
+        <code>
+        rp-	Defines a fallback parenthesis for a ruby annotation
+        </code>
+
+        <code>
+        rt-	Defines ruby text for Asian characters
+        </code>
+
+        <code>
+        ruby-	Represents ruby annotation for Asian characters
+        </code>
+
+        <h2>S</h2>
+
+        <code>
+        s-	Draws a line across the text making a strikethrough
+        </code>
+
+        <code>
+        samp-	Defines sample output from a computer program
+        </code>
+
+        <code>
+        script-	Defines a client-side script (JavaScript)
+        </code>
+
+        <code>
+        section-	Defines the primary content of the document
+        </code>
+
+        <code>
+        select-	Creates an HTML dropdown list with one or more options for a web form
+        </code>
+
+        <code>
+        small-	Reduces the HTML text size down by one size        
+        </code>
+
+        <code>
+        source-	Defines sources for media elements like audio, video and picture
+        </code>
+
+        <code>
+        span-	Sets an inline container for formatting an HTML document
+        </code>
+
+        <code>
+        strike-	DEPRECATED. Defined a portion of a text to be struck out
+        </code>
+
+        <code>
+        strong-	Makes an important part of text appear bold
+        </code>
+
+        <code>
+        style-	Defines styling properties for different HTML elements
+        </code>
+
+        <code>
+        sub-	Defines subscript text
+        </code>
+
+        <code>
+        summary-	Sets the summary content for the details element
+        </code>
+
+        <code>
+        sup-	Defines superscript text
+        </code>
+
+
+
+
 
       </div>
 
