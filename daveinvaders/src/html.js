96881f4300fdcf2e9a01c486fb6980a1b7701ce1
David Lee Clark <davetheclark87@gmail.com>
2022-09-20 00:54:13 -0600

    dave5

--- daveinvaders/src/html.js
+++ daveinvaders/src/html.js
@@ -26,7 +26,260 @@ class Html extends Component {
       
     <main>
 
-      
+      <div>
+        <h2>What is Html?</h2>
+        
+        <p>The HyperText Markup Language or HTML is the standard markup language for documents 
+        designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.
+        </p>
+        
+        <p>
+        Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages.
+        HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+        </p>
+
+        <i class='fa fa-html5' href='https://en.wikipedia.org/wiki/HTML'></i>
+
+        <h2>TAGS</h2>
+
+        
+        <code>
+        a-  Sets a hyperlink to another page  
+        </code>
+
+        <p></p>
+        <code>
+        abbr-	Represents an abbreviation or acronym and provides a full-text explanation to it (optional)   
+        </code>
+
+        <p></p>
+        <code>
+        acronym-	DEPRECATED. Defined an acronym and associated an explanation to it (optional)   
+        </code>
+
+        <p></p>
+        <code>
+        address-	Defines the contact information of the document's author.  
+        </code>
+
+        <p></p>
+        <code>
+        applet-	DEPRECATED. Embedded a Java applet into an HTML document   
+        </code>
+
+        <code>
+        area-	Defines a clickable area in an image map
+        </code>
+
+        <code>
+        article-	Defines independent content    
+        </code>
+
+        <code>
+        aside-	Describes content to be displayed aside other content    
+        </code>
+
+        <code>
+        audio-	Includes sound in the web page (music, streams, etc.)
+        </code>
+
+        
+
+        <h2>B</h2>
+
+        <code>
+        b-	Displays enclosed text in bold typeface without conveying any added importance
+        </code>
+
+        <code>
+        base-	Defines the base URI or URL for all the relative links in the HTML document
+        </code>
+
+        <code>
+        basefont-	DEPRECATED. Defined a default font-family, font-size, text-color for all the text in a document
+        </code>
+
+        <code>
+        bdi-	Defines bidirectional text isolation
+        </code>
+
+        <code>
+        bdo-	Defines bidirectional text override
+        </code>
+
+        <code>
+        big-	DEPRECATED. Defined a bigger font size in HTML
+        </code>
+
+        <code>
+        blink-	DEPRECATED. Defined text to blink
+        </code>
+
+        <code>
+        blockquote-	Defines a quoted section
+        </code>
+
+        <code>
+        body-	Defines the HTML container that contains visible contents of an HTML document
+        </code>
+
+        <code>
+        br-	Defines a line break
+        </code>
+
+        <code>
+        button-	Creates a button    
+        </code>
+
+        <h2>C</h2>
+
+        <code>
+        canvas-	Defines an area of the webpage that becomes a space for rendering graphics
+        </code>
+
+        <code>
+        caption-	Defines an HTML table caption
+        </code>
+        
+        <code>
+        center-	DEPRECATED. Set the alignment of content to the center
+        </code>
+
+        <code>
+        cite-	Defines a title for a work
+        </code>
+
+        <code>
+        code-	Defines text to be displayed in the computer output style
+        </code>
+
+        <code>
+        col-	Specifies grouped attributes for columns
+        </code>
+
+        <code>
+        colgroup-	Specifies a common formatting style for a group of columns
+        </code>
+
+        <code>
+        comment-	Specifies a comment in the document's source code  
+        </code>
+
+        <h2>D</h2>
+
+        <code>
+
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <h2>E</h2>
+
+        <code>
+
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <h2>F</h2>
+
+        <code>
+
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <code>
+        
+        </code>
+
+        <h2>G</h2>
+
+        <code>
+
+        </code>
+
+        <h2>H</h2>
+
+        <code>
+
+        </code>
+
+        <h2>I</h2>
+
+        <code>
+
+        </code>
+
+        <h2>J</h2>
+
+        <code>
+
+        </code>
+
+        <h2>K</h2>
+
+        <code>
+
+        </code>
+
+        <h2>Language</h2>
+
+        <code>
+
+        </code>
+
+      </div>
 
 
     </main>
