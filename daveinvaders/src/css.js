f129bcbb320e417203c6c148709c91cb5a068493
David Lee Clark <davetheclark87@gmail.com>
2022-09-20 19:25:48 -0600

    dave10

--- daveinvaders/src/css.js
+++ daveinvaders/src/css.js
@@ -40,9 +40,11 @@ class Css extends Component {
     enable multiple web pages to share formatting by specifying the relevant CSS in a separate .css file, which reduces complexity and repetition in the structural content; and enable the .css file to be cached to improve the page load speed between the pages that share the file and its formatting.    
     </p>
 
-    
+
 
     <h2>( Property List )</h2>
+
+    <h2>A</h2>
 
     <p></p>
     <code>
@@ -72,6 +74,653 @@ class Css extends Component {
     <p></p>
     <code>
     animation-direction- Specifies whether the animation should play in reverse on alternate cycles or not.
+    </code>
+
+    <p></p>
+    <code>
+    animation-duration- Specifies the number of seconds or milliseconds an animation should take to complete one cycle.
+    </code>
+
+    <p></p>
+    <code>
+    animation-fill-mode- Specifies how a CSS animation should apply styles to its target before and after it is executing.   
+    </code>
+
+    <p></p>
+    <code>
+    animation-iteration-count- Specifies the number of times an animation cycle should be played before stopping.
+    </code>
+
+    <p></p>
+    <code>
+    animation-name- Specifies the name of @keyframes defined animations that should be applied to the selected element.
+    </code>
+
+    <p></p>
+    <code>
+    animation-play-state- Specifies whether the animation is running or paused. 
+    </code>
+
+    <p></p>
+    <code>
+    animation-timing-function- Specifies how a CSS animation should progress over the duration of each cycle.
+    </code>
+
+    <h2>B</h2>
+
+    <p></p>
+    <code>
+    backface-visibility- Specifies whether or not the "back" side of a transformed element is visible when facing the user.
+    </code>
+
+    <p></p>
+    <code>
+    background- Defines a variety of background properties within one declaration.
+    </code>
+
+    <p></p>
+    <code>
+    background-attachment- Specify whether the background image is fixed in the viewport or scrolls. 
+    </code>
+
+    <p></p>
+    <code>
+    background-clip- Specifies the painting area of the background.
+    </code>
+
+    <p></p>
+    <code>
+    background-color- Defines an element's background color.
+    </code>
+
+    <p></p>
+    <code>
+    background-image- Defines an element's background image. 
+    </code>
+
+    <p></p>
+    <code>
+    background-origin- Specifies the positioning area of the background images.
+    </code>
+
+    <p></p>
+    <code>
+    ackground-position- Defines the origin of a background image.
+    </code>
+
+    <p></p>
+    <code>
+    background-repeat- Specify whether/how the background image is tiled.  
+    </code>
+
+    <p></p>
+    <code>
+    background-size- Specifies the size of the background images.
+    </code>
+
+    <p></p>
+    <code>
+    border- Sets the width, style, and color for all four sides of an element's border.
+    </code>
+
+    <p></p>
+    <code>
+    border-bottom- Sets the width, style, and color of the bottom border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-bottom-color- Sets the color of the bottom border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-bottom-left-radius- Defines the shape of the bottom-left border corner of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-bottom-right-radius- Defines the shape of the bottom-right border corner of an element. 
+    </code>
+
+    <p></p>
+    <code>
+    border-bottom-style- Sets the style of the bottom border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-bottom-width- Sets the width of the bottom border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-collapse- Specifies whether table cell borders are connected or separated.
+    </code>
+
+    <p></p>
+    <code>
+    border-color- Sets the color of the border on all the four sides of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-image- Specifies how an image is to be used in place of the border styles.
+    </code>
+
+    <p></p>
+    <code>
+    border-image-outset- Specifies the amount by which the border image area extends beyond the border box. 
+    </code>
+
+    <p></p>
+    <code>
+    border-image-repeat- Specifies whether the image-border should be repeated, rounded or stretched.
+    </code>
+
+    <p></p>
+    <code>
+    border-image-slice- Specifies the inward offsets of the image-border.
+    </code>
+
+    <p></p>
+    <code>
+    border-image-source- Specifies the location of the image to be used as a border.
+    </code>
+
+    <p></p>
+    <code>
+    border-image-width- Specifies the width of the image-border.
+    </code>
+
+    <p></p>
+    <code>
+    border-left- Sets the width, style, and color of the left border of an element. 
+    </code>
+
+    <p></p>
+    <code>
+    border-left-color- 	Sets the color of the left border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-left-style- Sets the style of the left border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-left-width- Sets the width of the left border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-radius- Defines the shape of the border corners of an element. 
+    </code>
+
+    <p></p>
+    <code>
+    border-right- Sets the width, style, and color of the right border of an element.  
+    </code>
+
+    <p></p>
+    <code>
+    border-right-color- Sets the color of the right border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-right-style- Sets the style of the right border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-right-width- Sets the width of the right border of an element.  
+    </code>
+
+    <p></p>
+    <code>
+    border-spacing- Sets the spacing between the borders of adjacent table cells.  
+    </code>
+
+    <p></p>
+    <code>
+    border-style- Sets the style of the border on all the four sides of an element. 
+    </code>
+
+    <p></p>
+    <code>
+    border-top- Sets the width, style, and color of the top border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-top-color- Sets the color of the top border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-top-left-radius- Defines the shape of the top-left border corner of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-top-right-radius- Defines the shape of the top-right border corner of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-top-style- Sets the style of the top border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-top-width- Sets the width of the top border of an element.
+    </code>
+
+    <p></p>
+    <code>
+    border-width- Sets the width of the border on all the four sides of an element.
+    </code>
+
+    <p></p>
+    <code>
+    bottom- Specify the location of the bottom edge of the positioned element.
+    </code>
+
+    <p></p>
+    <code>
+    box-shadow- Applies one or more drop-shadows to the element's box.
+    </code>
+
+    <p></p>
+    <code>
+    box-sizing- 	Alter the default CSS box model.
+    </code>
+
+    <h2>C</h2>
+
+    <p></p>
+    <code>
+    caption-side- Specify the position of table's caption.
+    </code>
+
+    <p></p>
+    <code>
+    clear- Specifies the placement of an element in relation to floating elements. ( clear: fix; )
+    </code>
+
+    <p></p>
+    <code>
+    clip- Defines the clipping region. 
+    </code>
+
+    <p></p>
+    <code>
+    color- Specify the color of the text of an element.
+    </code>
+
+    <p></p>
+    <code>
+    column-count- Specifies the number of columns in a multi-column element.
+    </code>
+
+    <p></p>
+    <code>
+    column-fill- Specifies how columns will be filled.
+    </code>
+
+    <p></p>
+    <code>
+    column-gap- Specifies the gap between the columns in a multi-column element.
+    </code>
+
+    <p></p>
+    <code>
+    column-rule- Specifies a straight line, or "rule", to be drawn between each column in a multi-column element.
+    </code>
+
+    <p></p>
+    <code>
+    column-rule-color- Specifies the color of the rules drawn between columns in a multi-column layout.
+    </code>
+
+    <p></p>
+    <code>
+    column-rule-style- Specifies the style of the rule drawn between the columns in a multi-column layout.
+    </code>
+
+    <p></p>
+    <code>
+    column-rule-width- Specifies the width of the rule drawn between the columns in a multi-column layout.
+    </code>
+
+    <p></p>
+    <code>
+    column-span- Specifies how many columns an element spans across in a multi-column layout.
+    </code>
+
+    <p></p>
+    <code>
+    column-width- Specifies the optimal width of the columns in a multi-column element.
+    </code>
+
+    <p></p>
+    <code>
+    columns- A shorthand property for setting column-width and column-count properties.
+    </code>
+
+    <p></p>
+    <code>
+    content- Inserts generated content.
+    </code>
+
+    <p></p>
+    <code>
+    counter-increment- 	Increments one or more counter values.
+    </code>
+
+    <p></p>
+    <code>
+    counter-reset- Creates or resets one or more counters.
+    </code>
+
+    <p></p>
+    <code>
+    cursor- Specify the type of cursor.
+    </code>
+
+    <h2>D</h2>
+
+    <p></p>
+    <code>
+    direction- Define the text direction/writing direction.
+    </code>
+
+    <p></p>
+    <code>
+    display- Specifies how an element is displayed onscreen.
+    </code>
+
+    <h2>E</h2>
+
+    <p></p>
+    <code>
+    empty-cells- Show or hide borders and backgrounds of empty table cells.
+    </code>
+
+    <p></p>
+    <code>
+    flex- Specifies the components of a flexible length. 
+    </code>
+
+    <p></p>
+    <code>
+    flex-basis- Specifies the initial main size of the flex item. 
+    </code>
+
+    <p></p>
+    <code>
+    flex-direction- 	Specifies the direction of the flexible items.
+    </code>
+
+    <p></p>
+    <code>
+    flex-flow- A shorthand property for the flex-direction and the flex-wrap properties.
+    </code>
+
+    <p></p>
+    <code>
+    flex-grow- Specifies how the flex item will grow relative to the other items inside the flex container. 
+    </code>
+
+    <p></p>
+    <code>
+    flex-shrink- Specifies how the flex item will shrink relative to the other items inside the flex container.
+    </code>
+
+    <p></p>
+    <code>
+    flex-wrap- 	Specifies whether the flexible items should wrap or not.
+    </code>
+
+    <p></p>
+    <code>
+    float- Specifies whether or not a box should float.
+    </code>
+
+    <p></p>
+    <code>
+    font- Defines a variety of font properties within one declaration.
+    </code>
+
+    <p></p>
+    <code>
+    font-family- Defines a list of fonts for element.
+    </code>
+
+    <p></p>
+    <code>
+    font-size- Defines the font size for the text.
+    </code>
+
+    <p></p>
+    <code>
+    font-size-adjust- Preserves the readability of text when font fallback occurs.  
+    </code>
+
+    <p></p>
+    <code>
+    font-stretch- Selects a normal, condensed, or expanded face from a font.
+    </code>
+
+    <p></p>
+    <code>
+    font-style- Defines the font style for the text. 
+    </code>
+
+    <p></p>
+    <code>
+    font-variant- Specify the font variant.
+    </code>
+
+    <p></p>
+    <code>
+    font-weight- Specify the font weight of the text.
+    </code>
+
+    <h2>H</h2>
+
+    <p></p>
+    <code>
+    height- Specify the height of an element.
+    </code>
+
+    <h2>J</h2>
+
+    <p></p>
+    <code>
+    justify-content- Specifies how flex items are aligned along the main axis of the flex container after any flexible lengths and auto margins have been resolved. 
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
+    </code>
+
+    <p></p>
+    <code>
+        
     </code>
 
     <p></p>
